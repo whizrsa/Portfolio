237ca8dd5ee252b78e482dd55fccad5f1774c80f
Elvis Chimuse <elvischimuse@gmail.com>
2025-03-13 13:00:29 +0200

    Form Update

--- email.js
+++ email.js
@@ -1,7 +1,9 @@
 function sendEmail()
 {
     event.preventDefault()
-    
+
+    const form = document.forms['submit-email']
+
     let params = {
         from_name : document.getElementById("name").value,
         from_email : document.getElementById("email").value,
@@ -12,6 +14,10 @@ function sendEmail()
         .then((response) => {
             console.log("SUCCESS", response);
             document.getElementById("msg").innerText = "Your message has been sent successfully!";
+            setTimeout(function(){
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
         }, (error) => {
             console.log("FAILED", error);
             document.getElementById("msg").innerText = "There was an error sending your message. Please try again.";
