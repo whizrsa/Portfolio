d08e6da660ea7d48f5415bd6b1a8bc707ac8bee4
Elvis Chimuse <elvischimuse@gmail.com>
2025-03-13 12:35:24 +0200

    Update Email

--- email.js
+++ email.js
@@ -6,7 +6,7 @@ function sendEmail()
         message : document.getElementById("message").value,
     }
 
-    emailjs.send("service_m2bvp1l", "template_6xsx27l", templateParams)
+    emailjs.send("service_m2bvp1l", "template_6xsx27l", params)
         .then((response) => {
             console.log("SUCCESS", response);
             document.getElementById("msg").innerText = "Your message has been sent successfully!";
